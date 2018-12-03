a7bd023e053f68a4d522d53f964ac662bb12f311
chenyg1119 <chenyg1119@hotmail.com>
2018-12-03 21:49:29 +0100

    typo

--- tunneling.py
+++ tunneling.py
@@ -8,13 +8,12 @@ d=2 #quantum dot energy
 theta = 0.2 #theta parameter
 w = np.linspace(-3,3,1000) #weyl point energy
 def a(value):
-    ***
-    calculating three roots of the odd parity tunneling matrix, with no spin splitting
+    '''calculating three roots of the odd parity tunneling matrix, with no spin splitting
     and value as the theta parameter.
     sorting the roots in descending order as a[0], a[1], a[2]
     --------------------
     returns a
-    ***
+    '''
     m = np.empty([3,1000])
     for i in range(1000):
         m[0,i] = np.sort(np.roots([1,-4,3-(w**2)[i],2-w[i]*np.cos(value)]))[0]
@@ -36,11 +35,11 @@ theta_slider_ax  = fig.add_axes([0.25, 0.15, 0.65, 0.03]) #draw slider
 theta_slider = Slider(theta_slider_ax, r'$\theta$', 0.1, 10.0, valinit=theta_0)
 
 def sliders_on_changed(val):
-    ***
+    '''
     activate the controling of the parameter theta
     ------------
     val = theta
-    ***
+    '''
     line1.set_ydata(a(theta_slider.val))
     line2.set_ydata(a(theta_slider.val))
     line3.set_ydata(a(theta_slider.val))
